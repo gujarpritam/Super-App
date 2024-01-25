f39c4819917e6b9220a96d7c66cde8969bcc0bd9
Pritam <pritamgujar27@gmail.com>
2024-01-25 13:13:45 +0530

    Added Routing

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Showcase from "./pages/Showcase";
+import Info from "./pages/Info";
+import Movies from "./pages/Movies";
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Register />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/info" element={<Info />}></Route>
+      <Route path="/movies" element={<Movies />}></Route>
+      <Route path="/showcase" element={<Showcase />}></Route>
+      <Route path="*" element={<h1>404 Route not found </h1>}></Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/Showcase/index.jsx
@@ -0,0 +1,9 @@
+function Showcase() {
+  return (
+    <div>
+      <h1>Showcase</h1>
+    </div>
+  );
+}
+
+export default Showcase;
